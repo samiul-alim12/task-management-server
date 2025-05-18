35ab7425c49f5dedd583cd20be41e536ec3ee183
samiul-alim12 <nawabitsamiul@gmail.com>
2025-05-19 02:31:00 +0600

    server side all work done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 // task-management-DB-user
@@ -24,6 +24,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("task_management_DB");
+    const taskCollection = database.collection("tasks");
+
+    app.get("/tasks", async (req, res) => {
+      const { searchParams } = req.query;
+      let query = {};
+      if (searchParams) {
+        query = { title: { $regex: searchParams, $options: "i" } };
+      }
+      const cursor = taskCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const tasksData = req.body;
+      const result = await taskCollection.insertOne(tasksData);
+      res.send(result);
+      //   console.log(tasksData);
+    });
+
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const task = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: task,
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
